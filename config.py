535ea0c36f0a0c32b502874dbffebb22ac0a438b
msbizrise-arch <msbizrise@gmail.com>
2026-06-24 09:57:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,22 +3,21 @@ import os
 API_ID = int(os.environ.get("API_ID", "38498066"))
 API_HASH = os.environ.get("API_HASH", "c9696114751feacdeb1b4487f5839a1a")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-THUMB_URL = "https://graph.org/file/5e7dc66913185f41c81ce-78485dba7e5ae7fc8e.jpg"
+THUMB_URL = "https://graph.org/file/673f55a25e28da21ade36-cc0b5c430f9666323e.jpg"
 THUMB_PATH = "document_thumb_v2.jpg"
 
-OWNER = int(os.environ.get("OWNER", "8446475678"))
+OWNER = int(os.environ.get("OWNER", "8909902924"))
 
 AUTH_USER = os.environ.get(
     "AUTH_USERS",
-    "6660248311,6446087354,8480660521,8680968748,8446475678,7988815969,8429278856,7920113547,8723278238"
-).split(',')
+    "6660248311,6446087354,8480660521,8680968748,8446475678,7988815969,8429278856,7920113547,8723278238,8715662594,8838086114").split(',')
 AUTH_USERS = [int(uid) for uid in AUTH_USER if uid.strip()]
 if OWNER not in AUTH_USERS:
     AUTH_USERS.append(OWNER)
 
 # ── Logging Channel ────────────────────────────────────────────────────────
 # Format: -100123456789 (negative group chat ID)
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003597599758"))
+LOG_CHANNEL = (os.environ.get("LOG_CHANNEL", "-1004370124638"))
 
 # ── Video Player Configuration ─────────────────────────────────────────────
 HEROKU_VIDEO_URL = os.environ.get(
